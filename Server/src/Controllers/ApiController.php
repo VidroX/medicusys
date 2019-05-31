1352d95c159426bb7157ef03c6fc91f454230d22
Vadym Karachenko <v1dr00x@gmail.com>
2019-05-31 14:17:08 +0300

    API security implementation

--- Server/src/Controllers/ApiController.php
+++ Server/src/Controllers/ApiController.php
@@ -12,10 +12,12 @@ class ApiController {
 
     private $i18n;
     private $config;
+    private $token;
 
     public function __construct(i18n $i18n) {
         $this->i18n = $i18n;
         $this->config = include(__DIR__."/../../config/core.php");
+        $this->token = $this->config['api']['token'];
     }
 
     public function index(Request $request, Response $response, $args = []){
@@ -24,52 +26,52 @@ class ApiController {
         ]);
     }
 
-    public function login(Request $request, Response $response, $args = []){
-        $user = new User();
-        if($user->isUserLoggedIn()) {
-            return $response->withJson([
-                "status"=>17,
-                "message"=>StatusCodes::STATUS[17]
-            ]);
-        }
-
+    public function login(Request $request, Response $response, $args = [])
+    {
         $parsedBody = $request->getParsedBody();
-        if(isset($parsedBody) && !empty($parsedBody)) {
-            if((isset($parsedBody['login']) && !empty($parsedBody['login'])) && (isset($parsedBody['password']) && !empty($parsedBody['password']))) {
-                $login = $parsedBody['login'];
-                $pass = $parsedBody['password'];
-
-                $user = $user->auth($login, $pass, true);
-                if (!($user instanceof User)) {
-                    $data = json_decode($user, true);
-                    if ($data['status'] === 2) {
-                        return $response->withJson([
-                            "status" => 2,
-                            "message" => StatusCodes::STATUS[2]
-                        ]);
-                    } elseif ($data['status'] === 1) {
-                        return $response->withJson([
-                            "status" => 1,
-                            "message" => StatusCodes::STATUS[1]
-                        ]);
-                    }
-                }
-
-                return $response->withJson([
-                    "status" => 18,
-                    "message" => StatusCodes::STATUS[18],
-                    "data" => $user->getData()
-                ]);
-            }else{
-                return $response->withJson([
-                    "status" => 21,
-                    "message" => StatusCodes::STATUS[21]
-                ]);
-            }
-        }else{
+        if(!isset($parsedBody) || empty($parsedBody)){
             return $response->withJson([
                 "status" => 20,
                 "message" => StatusCodes::STATUS[20]
+            ]);
+        }
+        if (!isset($parsedBody['token']) || (isset($parsedBody['token']) && $parsedBody['token'] != $this->token)) {
+            return $response->withJson([
+                "status" => 22,
+                "message" => StatusCodes::STATUS[22]
+            ]);
+        }
+
+        $user = new User();
+        if ((isset($parsedBody['login']) && !empty($parsedBody['login'])) && (isset($parsedBody['password']) && !empty($parsedBody['password']))) {
+            $login = $parsedBody['login'];
+            $pass = $parsedBody['password'];
+
+            $user = $user->auth($login, $pass, true);
+            if (!($user instanceof User)) {
+                $data = json_decode($user, true);
+                if ($data['status'] === 2) {
+                    return $response->withJson([
+                        "status" => 2,
+                        "message" => StatusCodes::STATUS[2]
+                    ]);
+                } elseif ($data['status'] === 1) {
+                    return $response->withJson([
+                        "status" => 1,
+                        "message" => StatusCodes::STATUS[1]
+                    ]);
+                }
+            }
+
+            return $response->withJson([
+                "status" => 18,
+                "message" => StatusCodes::STATUS[18],
+                "data" => $user->getData()
+            ]);
+        } else {
+            return $response->withJson([
+                "status" => 21,
+                "message" => StatusCodes::STATUS[21]
             ]);
         }
     }
