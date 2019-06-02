c1bd97ba5d4e03959cc28ea0bc2fb3a97db7a8c6
Yaroslav Honchar <yaroslav.honchar@nure.ua>
2019-06-02 17:35:24 +0300

    Android authorization update

--- Medicus System/app/src/main/java/me/medicusys/medicussystem/MainActivity.java
+++ Medicus System/app/src/main/java/me/medicusys/medicussystem/MainActivity.java
@@ -17,6 +17,6 @@ public class MainActivity extends AppCompatActivity {
         String login = ((TextView)findViewById(R.id.loginInput)).getText().toString();
         String password = ((TextView)findViewById(R.id.passwordInput)).getText().toString();
         TextView debugLogInResponse = findViewById(R.id.debugLogInResponse);
-        new Network(debugLogInResponse, "UTF-8").execute("http://192.168.0.103/api/v1/login", "login=" + login + "&password=" + password);
+        new Network(debugLogInResponse, "UTF-8").execute("http://192.168.0.103/api/v1/login", "login=" + login + "&password=" + password + "&token=PUsecR0B6brOYUcrI9LhiXU8w5SlFRorlFrdrlV");
     }
 }
